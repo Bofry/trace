fa414b460f9c55a8390b36d4ece47f4a38557bdb
Wilhelm Liao <williaml.twcp@gmail.com>
2023-03-26 20:59:07 +0800

    WIP: add SeveritySpan.Disable()

--- severitySpan.go
+++ severitySpan.go
@@ -17,44 +17,46 @@ type SeveritySpan struct {
 	err       error
 
 	events []SpanEvent
+
+	disabled bool
 }
 
-// Context implements SpanContext
+func (s *SeveritySpan) Disable(disabled bool) {
+	s.disabled = disabled
+}
+
+func (s *SeveritySpan) IsDisabled() bool {
+	return s.disabled
+}
+
 func (s *SeveritySpan) Context() context.Context {
 	return s.ctx
 }
 
-// HasSpanID implements SpanContext
 func (s *SeveritySpan) HasSpanID() bool {
 	return s.span.SpanContext().HasSpanID()
 }
 
-// HasTraceID implements SpanContext
 func (s *SeveritySpan) HasTraceID() bool {
 	return s.span.SpanContext().HasTraceID()
 }
 
-// SpanID implements SpanContext
 func (s *SeveritySpan) SpanID() trace.SpanID {
 	return s.span.SpanContext().SpanID()
 }
 
-// TraceFlags implements SpanContext
 func (s *SeveritySpan) TraceFlags() trace.TraceFlags {
 	return s.span.SpanContext().TraceFlags()
 }
 
-// TraceID implements SpanContext
 func (s *SeveritySpan) TraceID() trace.TraceID {
 	return s.span.SpanContext().TraceID()
 }
 
-// TraceState implements SpanContext
 func (s *SeveritySpan) TraceState() trace.TraceState {
 	return s.span.SpanContext().TraceState()
 }
 
-// Inject implements SeveritySpanContext
 func (s *SeveritySpan) Inject(p propagation.TextMapPropagator, c propagation.TextMapCarrier) {
 	if p == nil {
 		p = otel.GetTextMapPropagator()
@@ -62,13 +64,15 @@ func (s *SeveritySpan) Inject(p propagation.TextMapPropagator, c propagation.Tex
 	p.Inject(s.ctx, c)
 }
 
-// Link implements SpanContext
 func (s *SeveritySpan) Link() Link {
 	return Link(trace.LinkFromContext(s.ctx))
 }
 
-// End implements SpanContext
 func (s *SeveritySpan) End(opts ...trace.SpanEndOption) {
+	if s.disabled {
+		return
+	}
+
 	for _, e := range s.events {
 		e.Flush()
 	}
@@ -87,21 +91,30 @@ func (s *SeveritySpan) End(opts ...trace.SpanEndOption) {
 	s.span.End(opts...)
 }
 
-// Tags implements SpanContext
 func (s *SeveritySpan) Tags(tags ...KeyValue) {
+	if !s.span.IsRecording() {
+		return
+	}
+
 	s.span.SetAttributes(tags...)
 }
 
-// Argv implements SpanContext
 func (s *SeveritySpan) Argv(v interface{}) {
+	if !s.span.IsRecording() {
+		return
+	}
+
 	kvset := expandObject(string(__ATTR_ARGV), v)
 	s.span.SetAttributes(
 		kvset...,
 	)
 }
 
-// Reply implements SpanContext
 func (s *SeveritySpan) Reply(code ReplyCode, v interface{}) {
+	if !s.span.IsRecording() {
+		return
+	}
+
 	kvset := expandObject(string(__ATTR_REPLY), v)
 	s.span.SetAttributes(
 		kvset...,
@@ -111,124 +124,124 @@ func (s *SeveritySpan) Reply(code ReplyCode, v interface{}) {
 	s.replyCode = code
 }
 
-// Err implements SpanContext
 func (s *SeveritySpan) Err(err error) {
-	if s.span.IsRecording() {
-		s.span.RecordError(err, trace.WithAttributes(
-			__ATTR_EVENT_SEVERITY.String(ERR.Name()),
-		), trace.WithStackTrace(true))
-
-		// output later
-		s.err = err
+	if !s.span.IsRecording() {
+		return
 	}
+
+	s.span.RecordError(err, trace.WithAttributes(
+		__ATTR_EVENT_SEVERITY.String(ERR.Name()),
+	), trace.WithStackTrace(true))
+
+	// output later
+	s.err = err
 }
 
-// Debug implements SpanContext
 func (s *SeveritySpan) Debug(message string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  DEBUG,
-			message:   fmt.Sprintf(message, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  DEBUG,
+		message:   fmt.Sprintf(message, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Info implements SpanContext
 func (s *SeveritySpan) Info(message string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  INFO,
-			message:   fmt.Sprintf(message, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  INFO,
+		message:   fmt.Sprintf(message, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Notice implements SpanContext
 func (s *SeveritySpan) Notice(message string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  NOTICE,
-			message:   fmt.Sprintf(message, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  NOTICE,
+		message:   fmt.Sprintf(message, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Warning implements SpanContext
 func (s *SeveritySpan) Warning(reason string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  WARN,
-			message:   fmt.Sprintf(reason, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  WARN,
+		message:   fmt.Sprintf(reason, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Crit implements SpanContext
 func (s *SeveritySpan) Crit(reason string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  CRIT,
-			message:   fmt.Sprintf(reason, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  CRIT,
+		message:   fmt.Sprintf(reason, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Alert implements SpanContext
 func (s *SeveritySpan) Alert(reason string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  ALERT,
-			message:   fmt.Sprintf(reason, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  ALERT,
+		message:   fmt.Sprintf(reason, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// Emerg implements SpanContext
 func (s *SeveritySpan) Emerg(reason string, v ...interface{}) SpanEvent {
-	if s.span.IsRecording() {
-		event := &SeverityEvent{
-			timestamp: time.Now(),
-			span:      s.span,
-			severity:  EMERG,
-			message:   fmt.Sprintf(reason, v...),
-		}
-		s.events = append(s.events, event)
-		return event
+	if !s.span.IsRecording() {
+		return nopEventInstance
 	}
-	return nopEventInstance
+
+	event := &SeverityEvent{
+		timestamp: time.Now(),
+		span:      s.span,
+		severity:  EMERG,
+		message:   fmt.Sprintf(reason, v...),
+	}
+	s.events = append(s.events, event)
+	return event
 }
 
-// otelSpan implements SeveritySpanContext
 func (s *SeveritySpan) otelSpan() trace.Span {
 	return s.span
 }
